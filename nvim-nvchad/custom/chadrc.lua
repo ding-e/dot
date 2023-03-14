a0051d3898f0bcbcea155ddfaeca20cfa4c9a762
DING.YI <2016669264@qq.com>
2023-03-14 17:38:39 +0800

    Update chadrc.lua

--- nvim-nvchad/custom/chadrc.lua
+++ nvim-nvchad/custom/chadrc.lua
@@ -7,6 +7,15 @@ local config = require "custom.config"
 
 local M = {}
 
+M.options = {
+   nvChad = {
+      -- update_url = "https://github.com/NvChad/NvChad",
+      -- update_branch = "main",
+      update_url = "https://github.com/ding-e/NvChad-1.0",
+      update_branch = "1.0",
+   },
+}
+
 M.ui = {
    theme = config.theme,
    theme_toggle = config.theme_toggle,
